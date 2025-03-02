c8bce9320cd9f7bec4f66fe826a1cad900b91e3b
PhamVanHung412004 <hp36780@gmail.com>
2025-03-02 18:24:45 +0700

    fix file KNN

--- KNN.py
+++ KNN.py
@@ -1,5 +1,19 @@
 from sklearn.cluster import KMeans
-from algorithm import pygame,calc_distance,array_counts,Linear_Search,check_value,Draw_ox_oy,COLORS,colors_init,Show_mouse,draw_rect_backgroud,Draw_ox_oy,screen
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+from algorithm import pygame
+from algorithm import calc_distance
+from algorithm import array_counts
+from algorithm import Linear_Search
+from algorithm import check_value
+from algorithm import Draw_ox_oy
+from algorithm import COLORS
+from algorithm import colors_init
+from algorithm import Show_mouse
+from algorithm import draw_rect_backgroud
+from algorithm import Draw_ox_oy
+from algorithm import screen
+
 colors = COLORS()
 COLORS_LABELS = colors_init(colors)
 
@@ -12,7 +26,8 @@ class Name_button:
                 k_knn: pygame.surface.Surface,
                 run_knn: pygame.surface.Surface,
                 deleter_labels: pygame.surface.Surface,
-                reset_button: pygame.surface.Surface) -> None:
+                reset_button: pygame.surface.Surface,
+                reset_algorithm: pygame.surface.Surface) -> None:
         self.K_Kmeans_button = K_Kmeans_button
         self.dau_cong = dau_cong
         self.dau_tru = dau_tru
@@ -21,6 +36,8 @@ class Name_button:
         self.run_knn = run_knn
         self.deleter_labels = deleter_labels
         self.reset_button = reset_button
+        self.reset_algorithm = reset_algorithm
+        
             
     def show_name_button(self) -> None: 
         screen.blit(self.K_Kmeans_button,(1230,25))
@@ -34,12 +51,29 @@ class Name_button:
         screen.blit(self.run_knn,(1225 + 30,267 + 60))
         screen.blit(self.deleter_labels,(1225,385))
         screen.blit(self.reset_button,(1270,445))
+        screen.blit(self.reset_algorithm,(1260,505))
 
+class Model_KNN:
+    def __init__(self, n_KNN: int, 
+                datas_train: list[list] = None,
+                datas_labels: list[list] = None,
+                datas_test: list[list] = None):
+        
+        self.n_KNN: int = n_KNN
+        self.datas_train: list[list] = datas_train
+        self.datas_labels: list[list] = datas_labels
+        self.datas_test: list[list] = datas_test
+
+    def labels_predict(self) -> np.array:
+        model = KNeighborsClassifier(n_neighbors=self.n_KNN)
+        model.fit(self.datas_train,self.datas_labels)
+        return model.predict(self.datas_test)
+        
 def Check_and_show(
         labels: list[int],
         value_check: int,
-        points: [int | float | list],
-        COLORS_LABELS: COLORS | dict) -> None:
+        points: list[int | float | list] = None,
+        COLORS_LABELS: COLORS | dict = None) -> None:
 
     if (type(COLORS_LABELS) == COLORS):
         try:
@@ -55,6 +89,8 @@ def Check_and_show(
         except Exception as e:
             print("Error: {}".format(e))
 
+
+
 pygame.init()
 test = 0
 labels = []
@@ -66,12 +102,15 @@ K_Kmeans = 0
 results = []
 runing = True
 points = []
+
 clock = pygame.time.Clock()
 font = pygame.font.SysFont('sans', 20)
 font1 = pygame.font.SysFont('sans', 30)
 font2 = pygame.font.SysFont('sans', 40)
 font3 = pygame.font.SysFont('sans', 50) 
 check = False
+
+
 
 while runing:
     clock.tick(60)
@@ -85,24 +124,24 @@ while runing:
     x = font.render("X", True, colors.BLACK)
     y = font.render("Y", True, colors.BLACK)
     O = font.render("0", True, colors.BLACK)
-    screen.blit(x, (1100, 605))
-    screen.blit(y, (30, 35))
-    screen.blit(O, (35, 590))
-
     up = font.render("▲", True, colors.BLACK)
     ngang = font.render("►", True, colors.BLACK)
-
     button_random = font1.render("RANDOM", True, colors.BLACK)
     button_run_kmeans = font1.render("RUN KMEANS", True, colors.BLACK)
-        
     dau_cong = font2.render("+", True, colors.BLACK)
     dau_tru = font2.render("-", True, colors.BLACK)
     run_knn = font1.render("RUN KNN" , True, colors.BLACK)
     deleter_labels = font1.render("DELETE LABEL", True, colors.BLACK)
     reset_button = font1.render("RESET" , True, colors.BLACK)
+    reset_algorithm = font1.render("Algorithm" , True, colors.BLACK)
     title = font3.render("Illustration of the k-nearest neighbors algorithm",True,colors.BLACK)
+
+    screen.blit(x, (1100, 605))
+    screen.blit(y, (30, 35))
+    screen.blit(O, (35, 590))
     screen.blit(title,(200,600))
-    # print(colors.BLACK)
+
+    # draw ox oy
     draw_ox_oy = Draw_ox_oy(50, 50, 50, 600, 50, 600, 1100, 600, colors.BLACK, up, ngang,screen)
     draw_ox_oy.show()
 
@@ -119,10 +158,6 @@ while runing:
     rect = draw_rect_backgroud(1225,140,170,50,colors)
     rect.show()
 
-    #button run kmeans
-    # rect = draw_rect_backgroud(1225,140,170,50,colors)
-    # rect.show()
-    
     #button thuat toan
     rect = draw_rect_backgroud(1225,200,170,50,colors)
     rect.show()
@@ -145,6 +180,10 @@ while runing:
     rect = draw_rect_backgroud(1225,440,170,50,colors)
     rect.show()
 
+    #button Algorithm
+    rect = draw_rect_backgroud(1225,500,170,50,colors)
+    rect.show()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             check = True
@@ -163,14 +202,14 @@ while runing:
             if (1225 <= x_mouse <= 1225 + 170 and 140 <= y_mouse <= 140 + 50):
                 print("run kmeans")
                 try:
-                    labels_index = []
                     test += 1
                     kmeans = KMeans(n_clusters=K_Kmeans).fit(points)
                     labels = kmeans.predict(points)
                     clusters = kmeans.cluster_centers_
+                    labels_index = [[points[i],labels[i]] for i in range(len(labels))]
 
-                    for i in range(len(labels)):
-                        labels_index.append([points[i],labels[i]])
+                    # for i in range(len(labels)):
+                    #     labels_index.append([points[i],labels[i]])
 
                 except Exception as e:
                     print("Error : {}".format(e))
@@ -195,7 +234,7 @@ while runing:
             #1225,320,170,50
             if (1225 <= x_mouse <= 1225 + 170 and 320 <= y_mouse <= 320 + 50):
                 try:
-                    print("Run KNN")
+                    print("Run KNN")                    
                     poins_news = []
                     results = []
                     list_point = []
@@ -220,13 +259,12 @@ while runing:
 
                         label = check_value(list_index_value_max,labels_distance) # O(mlog(n))
                         results.append(label) 
-                        labels_index.append([i,label])
-                    K_knn = 0
+                    print("button run: ",results)
+
                 except Exception as e:
                     print("Error: {}".format(e))
                     break
             
-            # 1225,380,170,50
             if (1225 <= x_mouse <= 1225 + 170 and 380 <= y_mouse <= 380 + 50):
                 try:
                     print("deleter labels")                    
@@ -251,6 +289,15 @@ while runing:
                 except Exception as e:
                     print("Error: {}".format(e))
                     break
+                # 1225,500,170,50
+            if (1225 <= x_mouse <= 1225 + 170 and 500 <= y_mouse <= 500 + 50):
+                # print("Button Algorithm")
+                results = []
+                datas_train = [labels_index[i][0] for i in range(len(labels_index))]
+                datas_labels = [labels_index[i][1] for i in range(len(labels_index))]
+                model = Model_KNN(K_knn,datas_train,datas_labels,list_labels_news)
+                results = model.labels_predict()
+                print("Button Algorithm",results)
 
     k_knn = font1.render("K KNN = " + str(K_knn), True, colors.BLACK)
     K_Kmeans_button = font1.render("n_clusters = " + str(K_Kmeans), True, colors.BLACK)               
@@ -262,7 +309,8 @@ while runing:
                               k_knn,
                               run_knn,
                               deleter_labels,
-                              reset_button
+                              reset_button,
+                              reset_algorithm
                               )
     name_button.show_name_button()
 
--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# Illustration_of_KNN_algorithm
-# Mọi người chạy file KNN.py
-# file algorithm.py là file khởi tạo các thuộc tính sẽ sử dụng
+### Giới thiệu về dự án:
+Dự án cá nhân của mình là mình sử dụng các kiến thức về lập trình và cấu trúc dữ liệu và giải thuật kèm theo các kiến thức về toán học để triển khai minh họa lại quá trình thuật toán KNN hoạt động ra sao mà không sử dụng đến thư viện chỉ dùng lập trình để minh họa lên cách thuật toán hoạt động.
+### Cách tổ chức dự án:
+```bash
+├── image          # ảnh demo 
+├── KNN.py          # file code minh họa thuật toán KNN 
+├── algorithm.py # file code chứa các thuật toán sử dụng đã code lại bằng tay
+└── requirements.txt # Các thư viện phụ thuộc
+```
+### Cách chạy dự án mở terminal hoặc cmd trên laptop và copy dòng lệnh dưới đây để cài các thư viện cần thiết: 
+```bash
+pip install -r requirements
+```
+
+### Sau đó chạy file KNN.py thì sẽ chạy được chương trình minh họa thuật toán KNN và cách tiến hành thuật toán hình ảnh sau khi chạy: 
+![Hình ảnh sau khi chạy thành công file KNN.py](image/image.png)
+[Video demo](https://youtu.be/2Ug3PSD5Zzw)
